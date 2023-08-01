f9fc6156749bd4af6e43b90952fe6b31f4ebc624
hsmin1206 <139456583+hsmin1206@users.noreply.github.com>
2023-08-01 14:30:52 +0900

    DAO, DAOImpl, VO, mybatis 재업로드

--- src/main/java/com/setbang/dao/MemberDAO.java
+++ src/main/java/com/setbang/dao/MemberDAO.java
@@ -2,16 +2,9 @@ package com.setbang.dao;
 
 import java.util.List;
 
-import com.setbang.domain.memberVO;
+import com.setbang.domain.MemberVO;
 
-public interface memberDAO {
-	public void insertMember(memberVO vo);
+public interface MemberDAO {
 	
-	public void updateMember(memberVO vo);
-	
-	public void deleteMember(memberVO vo);
-	
-	public memberVO getMember(memberVO vo);
-	
-	public List<memberVO> getMemberList(memberVO vo);
+	public MemberVO selectMember(MemberVO vo);
 }
