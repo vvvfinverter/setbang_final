c80ba95c42cbfec43fd54d76607f95d12a0413b1
04-06 <04-06@DESKTOP-8P55AO6>
2023-08-02 10:08:13 +0900

    Controller 이름 변경

---
+++ src/main/java/com/setbang/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.setbang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.setbang.domain.MemberVO;
+import com.setbang.service.MemberService;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	private MemberService memberService;
+	
+	
+	@RequestMapping("/selectMember.do")
+	public void selectMember(MemberVO vo, Model model) {
+		model.addAttribute("Member", memberService.selectMember(vo));
+	}
+
+}
